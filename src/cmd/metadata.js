5d2b34bbce9700dcfe381df3061442203585a4b7
Eileen <eileenylj@gmail.com>
2023-07-25 16:35:52 -0400

    refactor: update webhook example

--- src/cmd/metadata.js
+++ src/cmd/metadata.js
@@ -16,12 +16,12 @@ function handleMetadata(pluginRequest) {
   } else if (pluginRequest.args[0] === "--api") {
     pluginResponse.Metadata = {
       description: "The `api` subcommand of the sample js plugin is meant to create an api for a project via Kubebuilder. It scaffolds a single file: `apiFile`",
-      examples: `Scaffold with the GVK:\n$  kubebuilder create api --plugins=kb-js-plugin/v1 --group webapp --version v1 --kind Guestbook`
+      examples: `Scaffold with the GVK:\n$ kubebuilder create api --plugins=kb-js-plugin/v1 --group webapp --version v1 --kind Guestbook`
     };
   } else if (pluginRequest.args[0] === "--webhook") {
     pluginResponse.Metadata = {
       description: "The `webhook` subcommand  of the sample js plugin is meant to create a webhook for a project via Kubebuilder. It scaffolds a single file: `webhookFile`",
-      examples: `Scaffold with the defaults:\n$ kubebuilder create webhook --plugins=kb-js-plugin/v1`
+      examples: `Scaffold with the GVK:\n$ kubebuilder create webhook --plugins=kb-js-plugin/v1 --group webapp --version v1 --kind Guestbook`
     };
   } else {
     pluginResponse.error = true;
